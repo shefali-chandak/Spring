4ff40a684ada3d51cf8cd63da07a14db06d433a0
shefali-chandak <99164226+shefali-chandak@users.noreply.github.com>
2022-04-05 14:37:31 +0530

    Update Main.java

--- SpringDemo/src/main/java/com/Main.java
+++ SpringDemo/src/main/java/com/Main.java
@@ -3,6 +3,7 @@ package com;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+//ApplicationContext is IoC and Dependency injection is spring.xml
 public class Main {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
