b320dd78506c0d3259056a8f364a3c1526c61a51
shefali-chandak <99164226+shefali-chandak@users.noreply.github.com>
2022-04-05 09:33:34 +0530

    Add files via upload

--- SpringDemo/src/main/java/com/Main.java
+++ SpringDemo/src/main/java/com/Main.java
@@ -1,7 +1,5 @@
 package com;
 
-//A reference this points to another spring bean.
-//a value is a true value being inputted. E.g. "sharon" or "1"
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
