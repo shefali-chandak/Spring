4bf87a6244d0e51ccfeb9979a93f8949a1fd4229
shefali-chandak <99164226+shefali-chandak@users.noreply.github.com>
2022-04-07 11:41:14 +0530

    AutoWire all types

---
+++ Autowire_Spring/src/main/java/com/Address.java
@@ -0,0 +1,43 @@
+package com;
+
+public class Address {
+    private String hName;
+    private String city;
+    private String state;
+
+    public Address() {
+    }
+
+    public String gethName() {
+        return hName;
+    }
+
+    public void sethName(String hName) {
+        this.hName = hName;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "hName='" + hName + '\'' +
+                ", city='" + city + '\'' +
+                ", state='" + state + '\'' +
+                '}';
+    }
+}
